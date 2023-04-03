19334b2849f7faa9f047da4253f695f8ce3b3339
astragenius <darthmalak86@gmail.com>
2023-04-03 21:35:50 +0200

    add - new Project react-router App

---
+++ react-router-app/src/pages/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='about'>
+        <h2>About us</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda nemo voluptate nesciunt distinctio aperiam sed aliquam maiores dicta! Dolorem esse minima, dolor voluptatem amet tempora assumenda odit facilis hic! Reprehenderit!</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae fuga porro veritatis nesciunt adipisci odio expedita reiciendis dolore distinctio culpa officiis vel, tenetur debitis veniam quos molestiae quo! Suscipit, in.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur atque ducimus commodi nulla voluptas pariatur quisquam quod, adipisci, excepturi error accusamus? Veritatis ad ut consectetur, quam quae atque eius expedita!</p>
+        
+    </div>
+  )
+}
+
+export default About
---
+++ react-router-app/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa aperiam, minus exercitationem repudiandae voluptatem numquam quisquam dolor doloremque magni, dolore modi deleniti ut, quibusdam corrupti explicabo voluptate voluptatum odit mollitia.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel eius, libero consequatur repellat voluptatum ducimus deleniti minima, explicabo nobis minus enim, ea dolores ex ipsam! Ea eos nisi iure consequuntur.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, alias qui illum libero nam similique omnis quis et dolor ab molestiae excepturi neque placeat impedit provident quod nobis ex repellat?</p>
+    </div>
+  )
+}
+
+export default Home
