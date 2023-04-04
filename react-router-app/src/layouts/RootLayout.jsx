4bf76468785a3a92942d2f552919a694e6840dd7
astragenius <darthmalak86@gmail.com>
2023-04-04 22:17:28 +0200

    add - styling to faq/contact page - add route to faq/contact - and rout to help section

--- react-router-app/src/layouts/RootLayout.jsx
+++ react-router-app/src/layouts/RootLayout.jsx
@@ -8,6 +8,7 @@ const RootLayout = () => {
             <h1>Test React Router App</h1>
             <NavLink to='/'>Home</NavLink>
             <NavLink to="about">About</NavLink>
+            <NavLink to="help">Help</NavLink>
         </nav>
         </header>
         <main>
