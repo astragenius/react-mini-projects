380a60928ebb6d876f7d1f5586d54cb6faf3c2d6
astragenius <darthmalak86@gmail.com>
2023-04-04 21:48:23 +0200

    add - RootLayout component and import createBrowserRouter

--- react-router-app/src/App.jsx
+++ react-router-app/src/App.jsx
@@ -1,27 +1,28 @@
-import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link, NavLink, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
+import RootLayout from './layouts/RootLayout';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    
+    <Route path='/' element={<RootLayout/>}>
+      <Route index element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+    </Route>
+    
+  )
+)
 function App() {
 
 
   return (
-    <BrowserRouter>
-    <header>
-      <nav>
-        <h1>Test React Router App</h1>
-        <NavLink to='/'>Home</NavLink>
-        <NavLink to="about">About</NavLink>
-      </nav>
-    </header>
-      <main>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/about' element={<About/>}/>
-        </Routes>
-      </main>
-    </BrowserRouter>
     
-  )
+    
+      <RouterProvider router={router}/>
+    
+    
+  );
 }
 
 export default App
