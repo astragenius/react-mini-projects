1a11b74b7312d57284922b7ab0f17c9eb7251d27
astragenius <darthmalak86@gmail.com>
2023-04-07 15:10:39 +0200

    add - Breadcrumbs and styles to it

--- react-router-app/src/components/Breadcrumbs.jsx
+++ react-router-app/src/components/Breadcrumbs.jsx
@@ -1,12 +1,30 @@
-import { useLocation } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 
 const Breadcrumbs = () => {
     const location = useLocation()
 
+    let currentLink = '';
+
+    const crumbs = location.pathname.split('/')
+      .filter(crumb => crumb !== '')
+      .map(crumb => {
+        currentLink += `/${crumb}`
+
+        return (
+          <div className="crumb" key={crumb}>
+            <Link to={currentLink}>
+              {crumb}
+            </Link>
+          </div>
+        )
+      })
+
    
   return (
-    <div>Breadcrumbs</div>
+    <div className="breadcrumbs">
+      {crumbs}
+    </div>
   )
 }
 
